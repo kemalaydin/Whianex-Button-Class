9b2c4a0c68898546a68e16e8389b25282659fa72
kemalaydin <kemal@whianex.com>
2013-11-24 17:23:20 +0200

    Whianex Button Sınıfları
    Whianex içerisinde kullanılacak butonların sınıf ve renk tanımlamalrı

---
+++ button.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="tr">
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>
+            Whianex 
+        </title>
+	<link rel="stylesheet" type="text/css" href="button.css">
+	<style>
+	body{
+		padding: 20px;
+	}
+	</style>
+</head>
+<body>
+	<a href="#" class="button but-red">Kırmızı Buton Sınıfımız</a><br/><br/>
+	<a href="#" class="button but-green">Yeşil Buton Sınıfımız</a><br/><br/>
+	<a href="#" class="button but-orange">Turuncu Buton Sınıfımız</a><br/><br/>
+	<a href="#" class="button but-black">siyah Buton Sınıfımız</a><br/><br/>
+	<a href="#" class="button but-gray">Gri Buton Sınıfımız</a><br/><br/>
+	<a href="#" class="button but-blue">Mavi Buton Sınıfımız</a><br/><br/>
+	<a href="#" class="button but-green but-xxxxlarge">En Büyük Buton Sınıfımız</a><br/><br/>
+</body>
+</html>
