aa7bde0ceb907788d69aabe3a2b30ecd4e72a15c
kemalaydin <kemal@whianex.com>
2013-11-27 16:41:04 +0200

    Whianex [ 27.11.2013 ]
    Button ve input sınıfları yapıldı. Bazı düzenlemeler yapıldı ;
    - Buttonların altına ufak gölge eklendi
    - Buton sınıfına right ve left oranlı padding eklendi

--- button.php
+++ button.php
@@ -7,7 +7,7 @@
         <title>
             Whianex 
         </title>
-	<link rel="stylesheet" type="text/css" href="button.css">
+	<link rel="stylesheet" type="text/css" href="css/button.css">
 	<style>
 	body{
 		padding: 20px;
@@ -16,11 +16,18 @@
 </head>
 <body>
 	<a href="#" class="button but-red">Kırmızı Buton Sınıfımız</a><br/><br/>
+	<a href="#" class="button but-red but-xxsmall">Small Button Sınıfı</a><br/><br/>
+	<a href="#" class="button but-red but-xsmall">Small Button Sınıfı</a><br/><br/>
+	<a href="#" class="button but-red but-small">Small Button Sınıfı</a><br/><br/>
+	<a href="#" class="button but-red but-medium">Small Button Sınıfı</a><br/><br/>
+	<a href="#" class="button but-red but-normal">Small Button Sınıfı</a><br/><br/>
+
+
 	<a href="#" class="button but-green">Yeşil Buton Sınıfımız</a><br/><br/>
 	<a href="#" class="button but-orange">Turuncu Buton Sınıfımız</a><br/><br/>
 	<a href="#" class="button but-black">siyah Buton Sınıfımız</a><br/><br/>
 	<a href="#" class="button but-gray">Gri Buton Sınıfımız</a><br/><br/>
 	<a href="#" class="button but-blue">Mavi Buton Sınıfımız</a><br/><br/>
-	<a href="#" class="button but-green but-xxxxlarge">En Büyük Buton Sınıfımız</a><br/><br/>
+	<a href="#" class="button but-green but-xxxlarge">En Büyük Buton Sınıfımız</a><br/><br/>
 </body>
 </html>
---
+++ input.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="tr">
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>
+            Whianex 
+        </title>
+	<link rel="stylesheet" type="text/css" href="css/input.css">
+	<link rel="stylesheet" type="text/css" href="css/button.css">
+	<style>
+	body{
+		padding: 20px;
+	}
+	</style>
+</head>
+<body>
+	<input type="text" class="input-text inp-search" placeholder="Search" value=""/> <br/><br/><br/>
+	<input type="text" class="input-text inp-small" placeholder="t" value="" /><input type="text" class="input-text inp-normal" placeholder="t" value="" /><br/><br/>
+	<textarea class="input-text inp-textarea" placeholder="Bir Güncelleme Paylaş"></textarea><br/><br/>
+	<input type="button" class="button but-blue but-xlarge but-rlpad" value="Ödeme Yap">
+</body>
+</html>
